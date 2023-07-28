8b54acd287e4200661caf47a60f0cb393f619abb
Traton Gossink <79613749+TratonGossink@users.noreply.github.com>
2023-07-28 05:02:15 -0500

    Initial Commit

---
+++ Chatty_Chat_App/Chatty_Chat_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  Chatty_Chat_AppApp.swift
+//  Chatty_Chat_App
+//
+//  Created by Traton Gossink on 7/28/23.
+//
+
+import SwiftUI
+
+@main
+struct Chatty_Chat_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
